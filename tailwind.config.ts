eef03386aebfd569a65b78fb53dfe2b523dccd6e
Hamdani <hamdani@synergizetech.dev>
2024-11-30 23:38:49 +0800

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
+        sans: ['Poppins', 'sans-serif']
       },
-    },
+      colors: {
+        primary: '#405189',
+        secondary: '#abb9e8'
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { flatRoutes } from 'remix-flat-routes'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: true
   }
 }
 
@@ -16,9 +17,15 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
+        v3_lazyRouteDiscovery: true
       },
+      routes: async (defineRoutes) => {
+        return flatRoutes('routes', defineRoutes, { basePath: '/' })
+      }
     }),
-    tsconfigPaths(),
+    tsconfigPaths()
   ],
-});
+  server: {
+    port: 3000
+  }
+})
